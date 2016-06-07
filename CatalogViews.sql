18cb09765ab1ca3a1b430b6b071db12b13f98074
Lucker Xia <lucker.xia@missionsky.com>
2016-06-07 15:02:36 +0800

    updated catalog views

--- CatalogViews.sql
+++ CatalogViews.sql
@@ -1,11 +1,10 @@
-﻿-- 查看 MSSQL 所有数据库, sysdatabases 不同于 sys.databases
-select * from sys.databases
-select * from sysdatabases
+﻿-- 查看 MSSQL 所有数据库
+select * from sys.sysdatabases
 
 -- 判断某个库是否存在
-select count(*) as 是否存在 from sys.databases where name='master'
+select count(*) as 是否存在 from sys.sysdatabases where name='master'
 
-select count(*) as 数据库总数 from sys.databases 
+select count(*) as 数据库总数 from sys.sysdatabases 
 
 
 -- 查看所有表 sysobjects
@@ -34,3 +33,6 @@ select * from syscolumns where id=object_id('ap_check')
 select count(*) from syscolumns where id = object_id('ap_check')
 
 select * from sys.tables
+
+select * from sys.sysobjects
+select * from sysobjects
